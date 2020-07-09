a6bbdb074f56461fb68557876b6cc0451fe4a25e
jp3pe <rlaworhkd430430@gmail.com>
2020-07-09 14:00:46 +0900

    Add prototype push_button_h1 method fur modify text editor

--- main.py
+++ main.py
@@ -14,21 +14,65 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.cursor_start: int
+        self.cursor_end: int
 
+        # Buttons
+        # Connect h1 buttons to Slot
         self.button_h1 = self.findChild(QtWidgets.QPushButton, 'pushButton_H1')
         self.button_h1.clicked.connect(self.push_button_h1)
+
+        # self.button_h2 = self.findChild(QtWidgets.QPushButton, 'pushButton_H2')
+        # self.button_h2.clicked.connect(self.push_button_h2)
+        #
+        # self.button_h3 = self.findChild(QtWidgets.QPushButton, 'pushButton_H3')
+        # self.button_h3.clicked.connect(self.push_button_h3)
+        #
+        # self.button_h4 = self.findChild(QtWidgets.QPushButton, 'pushButton_H4')
+        # self.button_h4.clicked.connect(self.push_button_h4)
+        #
+        # self.button_h5 = self.findChild(QtWidgets.QPushButton, 'pushButton_H5')
+        # self.button_h5.clicked.connect(self.push_button_h5)
+        #
+        # self.button_h6 = self.findChild(QtWidgets.QPushButton, 'pushButton_H6')
+        # self.button_h6.clicked.connect(self.push_button_h6)
+
+        # Editor
+        # Connect text editor to Slot
         self.edit = self.findChild(QtWidgets.QTextEdit, 'textEdit')
         self.edit.selectionChanged.connect(self.handle_selection_changed)
 
-    @Slot()
-    def push_button_h1(self):
-        print("push_button_h1 method worked")
+        # mouse cursor
+        self.cursor = self.edit.textCursor()
 
     @Slot()
     def handle_selection_changed(self):
         cursor = self.edit.textCursor()
-        print("Selection start: %d end: %d" %
-              (cursor.selectionStart(), cursor.selectionEnd()))
+        self.cursor_start = cursor.selectionStart()
+        self.cursor_end = cursor.selectionEnd()
+
+    @Slot()
+    def push_button_h1(self):
+        previous_text: str = self.edit.toPlainText()
+        selected_text = self.cursor
+
+        # Change selected_text in previous text
+        # And replace plain text of edit with it.
+        # print(self.cursor_start)
+        # print(self.cursor_end)
+
+        if (self.cursor_start <= 0):
+            changed_text = '# ' + previous_text[self.cursor_start:]
+            print(changed_text)
+        elif (self.cursor_start > 0):
+            changed_text = previous_text[:self.cursor_start] + '# ' + previous_text[self.cursor_start:]
+            print(changed_text)
+
+        # changed_text = previous_text[:self.cursor_start] + \
+        #                "# " \
+        #                + selected_text + \
+        #                self.cursor_end
+        self.edit.setPlainText(changed_text)
 
 
 if __name__ == "__main__":
