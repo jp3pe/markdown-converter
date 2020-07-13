d58154ed782530dbf0389c255c83892a7bc5a9f9
jp3pe <rlaworhkd430430@gmail.com>
2020-07-13 15:40:43 +0900

    Add method to insert lists
    Add buttons for unordered list and ordered list
    Fix plain text button method to ignore space between words

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import sys
 import PySide2
 from PySide2 import QtWidgets
 from PySide2.QtCore import Slot
-
 from UiMainWindow import Ui_MainWindow
+
+
+def string_index_replacer(previous: str, incoming: str, index: int) -> str:
+    result = previous[:index]
+    result = result + incoming + previous[index + 1:]
+
+    return result
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -38,6 +44,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.button_h6 = self.findChild(QtWidgets.QPushButton, 'pushButton_H6')
         self.button_h6.clicked.connect(self.push_button_h6)
+
+        self.button_unordered_list = self.findChild(QtWidgets.QPushButton, 'pushButton_UnorderedList')
+        self.button_unordered_list.clicked.connect(self.push_button_unordered_list)
+
+        self.button_ordered_list = self.findChild(QtWidgets.QPushButton, 'pushButton_OrderedList')
+        self.button_ordered_list.clicked.connect(self.push_button_ordered_list)
 
         # Editor
         # Connect text editor to Slot
@@ -80,19 +92,27 @@ class MainWindow(QtWidgets.QMainWindow):
     def push_button_h6(self):
         self.h_styler(6)
 
-    # Method to insert H1~H6's prefix('#')
-    def h_insert(self, prefix: str, previous_text) -> str:
+    @Slot()
+    def push_button_unordered_list(self, prefix: str):
+        self.list_styler('*')
+
+    @Slot()
+    def push_button_ordered_list(self, prefix: str):
+        self.list_styler('1.')
+
+    # Method to insert prefix string like H1~H6's prefix('#') and lists' prefixes('*', '1.')
+    def insert_prefix(self, prefix: str, previous_text) -> str:
         # When user don't dragged any string
         if self.cursor_start == self.cursor_end:
             return previous_text
 
         # drag started from 0 index
         if self.cursor_start <= 0:
-            # return #s + user_input
+            # return prefix + user_input
             changed_text = prefix + ' ' + previous_text[self.cursor_start:]
         # drag start from bigger than 0 index
         elif self.cursor_start > 0:
-            # return user input + #s + behind user's dragged string
+            # return user input + prefix + behind user's dragged string
             changed_text = previous_text[:self.cursor_start] + prefix + ' ' + previous_text[self.cursor_start:]
 
         return changed_text
@@ -101,8 +121,20 @@ class MainWindow(QtWidgets.QMainWindow):
         # Change selected_text in previous text
         # And replace plain text of edit with it.
         previous_text: str = self.edit.toPlainText()
-        changed_text = self.h_insert('#' * h_num, previous_text)
+        changed_text = self.insert_prefix('#' * h_num, previous_text)
         self.edit.setPlainText(changed_text)
+
+    # method to call insert_prefix to insert prefix with list_prefixes
+    def list_styler(self, prefix: str):
+        # Change selected_text in previous text
+        # And replace plain text of edit with it.
+        previous_text: str = self.edit.toPlainText()
+        changed_text = self.insert_prefix(prefix, previous_text)
+        self.edit.setPlainText(changed_text)
+
+    # method to insert '* ' or '1. ' for ordered list and unordered list
+    def list_insert(self, prefix: str):
+        pass
 
     def style_remover(self, previous_text: str):
         # drag started from 0 index
@@ -110,12 +142,22 @@ class MainWindow(QtWidgets.QMainWindow):
         i = 0
         if self.cursor_start <= 0:
             # header delete
-            while temp_text[i] == '#':
-                temp_text = temp_text.replace('#', '')
-                i += 1
+            if temp_text[i] == '#':
+                while temp_text[i] == '#':
+                    temp_text = string_index_replacer(temp_text, '', i)
 
-            if temp_text[i - 1] == ' ':
-                temp_text = temp_text.replace(' ', '')
+                i = 0
+
+            # lists delete
+            if temp_text[i:i + 2] == '1.':
+                # call string_index_replacer twice to remove number and dot
+                temp_text = string_index_replacer(temp_text, '', i)
+                temp_text = string_index_replacer(temp_text, '', i)
+            if temp_text[i] == '*':
+                temp_text = string_index_replacer(temp_text, '', i)
+
+            if temp_text[i] == ' ':
+                temp_text = string_index_replacer(temp_text, '', i)
                 changed_text = temp_text
 
             self.edit.setPlainText(changed_text)
@@ -127,11 +169,18 @@ class MainWindow(QtWidgets.QMainWindow):
             i = self.cursor_start
 
             while temp_text[i] == '#':
-                temp_text = temp_text.replace('#', '')
-                i += 1
+                temp_text = string_index_replacer(temp_text, '', i)
 
-            if temp_text[i - 1] == ' ':
-                temp_text = temp_text.replace(' ', '')
+            # lists delete
+            if temp_text[i:i + 2] == '1.':
+                # call string_index_replacer twice to remove number and dot
+                temp_text = string_index_replacer(temp_text, '', i)
+                temp_text = string_index_replacer(temp_text, '', i)
+            if temp_text[i] == '*':
+                temp_text = string_index_replacer(temp_text, '', i)
+
+            if temp_text[i] == ' ':
+                temp_text = string_index_replacer(temp_text, '', i)
                 changed_text = changed_text + temp_text[self.cursor_start:]
 
             self.edit.setPlainText(changed_text)
